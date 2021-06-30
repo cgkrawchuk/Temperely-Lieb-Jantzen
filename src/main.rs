6c7c8c8a5e7b3e48b4450e8973a901db93d70109
cgkrawchuk <colinkwk@mymts.net>
2021-06-30 22:12:27 +0100

    closed issues and fixed dimension of new gram matrix

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate itertools;
 
 use itertools::Itertools;
-use std::io::{stdout, BufWriter};
 use temperley_lieb_cat::*;
 
-pub fn gramMatrix(n: usize, m: usize) -> Vec<Vec<i64>> {
+pub fn gram_matrix(n: usize, m: usize) -> Vec<Vec<i64>> {
     fn ok(tab: &Vec<usize>) -> bool {
         for (c, i) in tab.iter().enumerate() {
             if *i < 2 * (c + 1) {
@@ -13,11 +12,11 @@ pub fn gramMatrix(n: usize, m: usize) -> Vec<Vec<i64>> {
         }
         true
     }
-    let mut monicDiagrams = Vec::new();
+    let mut monic_diagrams = Vec::new();
     let prop = m;
     let rn = (n - prop) / 2;
     for lt in (1..(n + 1)).combinations(rn).filter(ok) {
-        monicDiagrams.push(TLDiagram::new(
+        monic_diagrams.push(TLDiagram::new(
             n,
             m,
             lt.iter().fold(0, |l, n| l | (1 << n)),
@@ -25,11 +24,11 @@ pub fn gramMatrix(n: usize, m: usize) -> Vec<Vec<i64>> {
         ));
     }
 
-    let x = monicDiagrams.len();
+    let x = monic_diagrams.len();
     let mut gm: Vec<Vec<i64>> = vec![vec![0; x as usize]; x as usize];
     for i in 0..x {
         for j in 0..x {
-            let (a, b) = &(monicDiagrams[i].involute()) * &monicDiagrams[j];
+            let (a, b) = &(monic_diagrams[i].involute()) * &monic_diagrams[j];
             if b == TLDiagram::id(m) {
                 if a == 0 {
                     gm[i][j] = 1
@@ -44,21 +43,21 @@ pub fn gramMatrix(n: usize, m: usize) -> Vec<Vec<i64>> {
     return gm;
 }
 
-pub fn rowEchelonForm(matrix: &mut Vec<Vec<i64>>, p: i64) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
+pub fn row_echelon_form(matrix: &mut Vec<Vec<i64>>, p: i64) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
     let mut matrix_out: Vec<Vec<i64>> = matrix.to_vec();
     let mut pivot = 0;
     let row_count = matrix_out.len();
     let column_count = matrix_out[0].len();
 
-    let mut identityMatrix: Vec<Vec<i64>> =
+    let mut identity_matrix: Vec<Vec<i64>> =
         vec![vec![0; column_count as usize]; row_count as usize];
     for k in 0..row_count {
         //hopefully row_count is less than column_count
-        identityMatrix[k][k] = 1;
+        identity_matrix[k][k] = 1;
     }
-    //println!("{:?}", identityMatrix);
+    //println!("{:?}", identity_matrix);
     for r in 0..row_count {
-        matrix_out = reduceModP(&mut matrix_out, p);
+        matrix_out = reduce_mod_p(&mut matrix_out, p);
         if column_count <= pivot {
             break;
         }
@@ -74,16 +73,16 @@ pub fn rowEchelonForm(matrix: &mut Vec<Vec<i64>>, p: i64) -> (Vec<Vec<i64>>, Vec
                 }
             }
         }
-        matrix_out = swapRows(&mut matrix_out, r, i, row_count);
-        identityMatrix = swapRows(&mut identityMatrix, r, i, row_count);
+        matrix_out = swap_rows(&mut matrix_out, r, i);
+        identity_matrix = swap_rows(&mut identity_matrix, r, i);
 
         let q = matrix_out[r][pivot];
         let a = ((q % p) + p) % p;
         if a != 0 {
-            let modInv = mod_inv((((a % p) + p) % p), p);
+            let mod_inverse = mod_inv(((a % p) + p) % p, p);
             for j in 0..column_count {
-                matrix_out[r][j] = matrix_out[r][j] * modInv;
-                identityMatrix[r][j] = identityMatrix[r][j] * modInv;
+                matrix_out[r][j] = matrix_out[r][j] * mod_inverse;
+                identity_matrix[r][j] = identity_matrix[r][j] * mod_inverse;
             }
         }
         for j in 0..row_count {
@@ -91,31 +90,30 @@ pub fn rowEchelonForm(matrix: &mut Vec<Vec<i64>>, p: i64) -> (Vec<Vec<i64>>, Vec
                 let hold = matrix_out[j][pivot];
                 for k in 0..column_count {
                     matrix_out[j][k] = matrix_out[j][k] - (hold * matrix_out[r][k]);
-                    identityMatrix[j][k] = identityMatrix[j][k] - (hold * identityMatrix[r][k]);
+                    identity_matrix[j][k] = identity_matrix[j][k] - (hold * identity_matrix[r][k]);
                 }
             }
         }
         pivot = pivot + 1;
     }
-    (matrix_out, identityMatrix)
+    (matrix_out, identity_matrix)
 }
 
-pub fn swapRows(
-    matrixIn: &mut Vec<Vec<i64>>,
+pub fn swap_rows(
+    matrix: & mut  Vec<Vec<i64>>,
     r: usize,
     i: usize,
-    row_count: usize,
 ) -> Vec<Vec<i64>> {
-    let mut matrix: Vec<Vec<i64>> = matrixIn.to_vec();
-    for j in 0..row_count {
+    let row_count = matrix.len();
+       for j in 0..row_count {
         let temp = matrix[r][j];
         matrix[r][j] = matrix[i][j];
         matrix[i][j] = temp;
     }
-    matrix
+    matrix.to_vec()
 }
 
-pub fn reduceModP(matrix: &Vec<Vec<i64>>, p: i64) -> Vec<Vec<i64>> {
+pub fn reduce_mod_p(matrix: &Vec<Vec<i64>>, p: i64) -> Vec<Vec<i64>> {
     let mut matrix_out: Vec<Vec<i64>> = matrix.to_vec();
     let row_count = matrix_out.len();
     let column_count = matrix_out[0].len();
@@ -128,6 +126,7 @@ pub fn reduceModP(matrix: &Vec<Vec<i64>>, p: i64) -> Vec<Vec<i64>> {
 }
 
 fn mod_inv(a: i64, module: i64) -> i64 {
+    assert!(a%module !=0, "number is 0 mod...");
     let mut mn = (module, a);
     let mut xy = (0, 1);
 
@@ -142,35 +141,33 @@ fn mod_inv(a: i64, module: i64) -> i64 {
     xy.0
 }
 
-pub fn numZeroRows(matrix: &Vec<Vec<i64>>) -> i64 {
-    let mut numZeroes = 0;
+pub fn num_zero_rows(matrix: &Vec<Vec<i64>>) -> i64 {
+    let mut num_zeroes = 0;
     let row_count = matrix.len();
     let column_count = matrix[0].len();
     let zero_vec = vec![0; column_count];
     for x in 0..row_count {
         if matrix[row_count - x - 1] == zero_vec {
-            numZeroes = numZeroes + 1;
+            num_zeroes = num_zeroes + 1;
         } else {
             break;
         } //remove for more general matrix
     }
-    return numZeroes;
+    return num_zeroes;
 }
 
 fn reform_inner_product(
-    new_basis: &mut Vec<Vec<i64>>,
-    old_gram: &mut Vec<Vec<i64>>,
+    new_basis: & Vec<Vec<i64>>,
+    old_gram: & Vec<Vec<i64>>,
 ) -> Vec<Vec<i64>> {
     let mut matrix_out: Vec<Vec<i64>> =
-        vec![vec![0; new_basis[0].len() as usize]; new_basis.len() as usize];
+        vec![vec![0; new_basis.len() ]; new_basis.len() ];
     for i in 0..new_basis.len() {
         for j in 0..new_basis.len() {
-            let basis_i = new_basis[i].clone();
-            let basis_j = new_basis[j].clone();
             let mut inner = 0;
             for k in 0..old_gram.len() {
                 for l in 0..old_gram[0].len() {
-                    inner += old_gram[k][l] * basis_i[k] * basis_j[l];
+                    inner += old_gram[k][l] * new_basis[i][k] * new_basis[j][l];
                 }
             }
             matrix_out[i][j] = inner;
@@ -180,41 +177,41 @@ fn reform_inner_product(
     matrix_out
 }
 
-fn recursiveOps(m: usize, n: usize, p: i64) {
-    let mut G = gramMatrix(m, n);
-    println!("Gram Matrix of {0}, {1} is :", m, n);
-    println!("{:?}", &G);
+fn recursive_ops(m: usize, n: usize, p: i64) {
+    let mut g = gram_matrix(m, n);
+    println!("gram Matrix of {0}, {1} is :", m, n);
+    println!("{:?}", &g);
 
-    let mut G = reduceModP(&mut G, p);
+    let mut g = reduce_mod_p(&mut g, p);
     println!("Reduced mod {}", p);
-    println!("{:?}", &G);
+    println!("{:?}", &g);
 
-    let (mut reducedMatrix, mut transformMatrix) = rowEchelonForm(&mut G, p);
+    let ( reduced_matrix,  transform_matrix) = row_echelon_form(&mut g, p);
 
-    println!("Row echelon form: \n{:?}", reduceModP(&reducedMatrix, p));
+    println!("Row echelon form: \n{:?}", reduce_mod_p(&reduced_matrix, p));
     println!(
         "transformation matrix:\n{:?}",
-        reduceModP(&transformMatrix, p)
+        reduce_mod_p(&transform_matrix, p)
     );
 
-    let mut rank = (reducedMatrix.len() as i64) - numZeroRows(&reducedMatrix.clone());
+    let mut rank = (reduced_matrix.len() as i64) - num_zero_rows(&reduced_matrix);
     println!("dimension of head is: {}", &rank);
 
     while rank != 0 as i64 {
-        let basisOfRad = &transformMatrix[(rank as usize)..];
-        let mut bor: Vec<Vec<i64>> = Vec::from(basisOfRad);
-        println!("new basis: {:?}", &basisOfRad);
-        let mut newG = reform_inner_product(&mut bor, &mut G);
-        println!("new Gram Matrix: {:?}", &newG);
+        let basis_of_rad = &transform_matrix[(rank as usize)..];
+        let mut bor: Vec<Vec<i64>> = Vec::from(basis_of_rad);
+        println!("new basis: {:?}", &basis_of_rad);
+        let mut newg = reform_inner_product(&mut bor, &mut g);
+        println!("new gram Matrix: {:?}", &newg);
 
-        let (mut reducedMatrix, mut transformMatrix) = rowEchelonForm(&mut newG, p);
-        println!("Row echelon form: \n{:?}", reduceModP(&reducedMatrix, p));
+        let ( reduced_matrix,  transform_matrix) = row_echelon_form(&mut newg, p);
+        println!("Row echelon form: \n{:?}", reduce_mod_p(&reduced_matrix, p));
         println!(
             "transformation matrix:\n{:?}",
-            reduceModP(&transformMatrix, p)
+            reduce_mod_p(&transform_matrix, p)
         );
 
-        rank = (reducedMatrix.len() as i64) - numZeroRows(&reducedMatrix.clone());
+        rank = (reduced_matrix.len() as i64) - num_zero_rows(&reduced_matrix.clone());
         println!("dimension of head is: {}", rank);
     }
 }
@@ -224,5 +221,5 @@ fn main() {
     let n = 4;
     let p = 3;
 
-    recursiveOps(m, n, p);
+    recursive_ops(m, n, p);
 }
